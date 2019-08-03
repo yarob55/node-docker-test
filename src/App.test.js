07e522538dd71826cb199e1d915fa3d6a5dcf5e5
Yarob Al Mostafa <yarob.hm@gmail.com>
2019-08-03 20:21:28 +0300

    adding test

--- src/App.test.js
+++ src/App.test.js
@@ -14,9 +14,4 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
 
